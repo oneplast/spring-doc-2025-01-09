9cabf516ff3f1499c3ee48002240f3e8efd2b697
Gyeonghun Min <riverpower6@naver.com>
2025-01-10 09:34:04 +0900

    9: refactor: default-produces-media-type / application/json;utf-8

--- backend/src/main/java/com/ll/springdoc20250109/domain/home/home/controller/HomeController.java
+++ backend/src/main/java/com/ll/springdoc20250109/domain/home/home/controller/HomeController.java
@@ -1,14 +1,18 @@
 package com.ll.springdoc20250109.domain.home.home.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Tag(name = "HomeController", description = "홈 컨트롤러")
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping(value = "/", produces = "text/html;charset=utf8")
     @ResponseBody
+    @Operation(summary = "메인 페이지")
     public String main() {
-        return "API 서버입니다.";
+        return "<h1>API 서버 입니다.</h1>";
     }
 }
