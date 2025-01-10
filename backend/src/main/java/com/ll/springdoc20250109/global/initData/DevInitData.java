8c03b8f82147a7915031fe14f1e7607424fb0b6c
Gyeonghun Min <riverpower6@naver.com>
2025-01-10 11:54:35 +0900

    17: add: api-docs - download from url to file

---
+++ backend/src/main/java/com/ll/springdoc20250109/global/initData/DevInitData.java
@@ -0,0 +1,30 @@
+package com.ll.springdoc20250109.global.initData;
+
+import com.ll.springdoc20250109.domain.member.member.service.MemberService;
+import com.ll.springdoc20250109.domain.post.post.service.PostService;
+import com.ll.springdoc20250109.util.Ut;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
+
+@Profile("dev")
+@Configuration
+@RequiredArgsConstructor
+public class DevInitData {
+    private final MemberService memberService;
+    private final PostService postService;
+    @Autowired
+    @Lazy
+    private DevInitData self;
+
+    @Bean
+    public ApplicationRunner devInitDataApplicationRunner() {
+        return args -> {
+            Ut.file.downloadByHttp("http://localhost:8080/v3/api-docs/apiV1", ".");
+        };
+    }
+}
